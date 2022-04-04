5aa580373efcecacfe4c1de94ef3a399395295e0
jeancaffou <jean@briskula.si>
2022-04-04 23:39:43 +0200

    proxy namespace fix

--- design.php
+++ design.php
@@ -4,7 +4,7 @@ header('Cache-Control: no-store, no-cache, must-revalidate');
 header('Cache-Control: post-check=0, pre-check=0', FALSE);
 header('Pragma: no-cache');
 header('Access-Control-Allow-Origin: *');
-header('Content-Type: image/png'); 
+header('Content-Type: image/png');
 
-echo file_get_contents('https://raw.githubusercontent.com/jeancaffou/Bot/master/design.png');
-//echo file_get_contents('design.png');
+echo file_get_contents('https://github.com/jeancaffou/r-place-bot/blob/master/design.png');
+
